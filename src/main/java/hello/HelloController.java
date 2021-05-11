988914e195016103be91a30c1ce1b1cfa08a74f7
HariharaSudhan0998 <70124601+HariharaSudhan0998@users.noreply.github.com>
2021-05-11 12:18:58 +0530

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return " Welcome to Flogile Technologies all !";
+        return " Welcome to Flogile Technologies  !";
     }
     
 }
